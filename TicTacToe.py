ab0a0bd65be011163e8a2ca020a9007f1b07c64f
sarath kumar <sarathkumark.s365@gmail.com>
2020-09-23 20:56:29 +0530

    initial version finished

--- TicTacToe.py
+++ TicTacToe.py
@@ -28,6 +28,49 @@ def current_player():
 display_b()
 
 
+def row_win():
+    if board[0]==board[1]==board[2]:
+        print("ROW 1 MATCHED")
+        print("PLAYER "+cp+" WON")
+        return 1
+    if board[3]==board[4]==board[5]:
+        print("ROW 2 MATCHED")
+        print("PLAYER "+cp+" WON")
+        return 1
+    if board[6]==board[7]==board[8]:
+        print("ROW 3 MATCHED")
+        print("PLAYER "+cp+" WON")
+        return 1
+
+
+
+
+def col_win():
+    if board[0]==board[3]==board[6]:
+        print("COL 1 MATCHED")
+        print("PLAYER "+cp+" WON")
+        return 1
+    if board[1]==board[4]==board[7]:
+        print("COL 2 MATCHED")
+        print("PLAYER "+cp+" WON")
+        return 1
+    if board[2]==board[5]==board[8]:
+        print("COL 3 MATCHED")
+        print("PLAYER "+cp+" WON")
+        return 1
+
+
+def diagonal_win():
+    if board[2]==board[4]==board[6]:
+        print("DIAGONAL 1 MATCHED")
+        print("PLAYER "+cp+" WON")
+        return 1
+    if board[0]==board[4]==board[8]:
+        print("ROW 2 MATCHED")
+        print("PLAYER "+cp+" WON")
+        return 1
+
+
 while(True):
     print("\n")
     cp =current_player()
@@ -36,42 +79,19 @@ while(True):
     usr_input = int(input(">>  "))
     board[usr_input-1] = cp
     display_b()
+    if col_win() == 1:
+        display_b()
+        break
+    elif row_win() == 1:
+        display_b()
+        break
+    elif diagonal_win() == 1:
+        display_b()
+        break
 
 
 
 
-def row_win():
-    if board[0]==board[1]==board[2]:
-        print("ROW 1 MATCHED")
-        print("PLAYER "+pwon+" WON")
-    if board[3]==board[4]==board[5]:
-        print("ROW 2 MATCHED")
-        print("PLAYER "+pwon+" WON")
-    if board[6]==board[7]==board[8]:
-        print("ROW 3 MATCHED")
-        print("PLAYER "+pwon+" WON")
-
-
-
-def col_win():
-    if board[0]==board[3]==board[6]:
-        print("COL 1 MATCHED")
-        print("PLAYER "+pwon+" WON")
-    if board[1]==board[4]==board[7]:
-        print("COL 2 MATCHED")
-        print("PLAYER "+pwon+" WON")
-    if board[2]==board[5]==board[8]:
-        print("COL 3 MATCHED")
-        print("PLAYER "+pwon+" WON")
-
-
-def diagonal_win():
-    if board[2]==board[4]==board[6]:
-        print("DIAGONAL 1 MATCHED")
-        print("PLAYER "+pwon+" WON")
-    if board[0]==board[4]==board[8]:
-        print("ROW 2 MATCHED")
-        print("PLAYER "+pwon+" WON")
 
 
        
